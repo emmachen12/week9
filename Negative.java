1331a69114e0dd199f4e03d29bef73b257059b0c
emmachen12 <emc1207pi@gmail.com>
2024-04-10 14:40:18 -0400

    neg

---
+++ Negative.java
@@ -0,0 +1,23 @@
+//Emma Chen
+// 4/10/24
+    public class Negative{
+    public static void main (String[]args){
+    int num1 = (int)(7*Math.random()-3);
+    int num2 = (int)(7*Math.random()-3);
+    int num3 = (int)(7*Math.random()-3);
+    int num4 = (int)(7*Math.random()-3);
+
+    int sum = num1 + num2 + num3 + num4;
+    if(sum<0){
+        System.out.println("Negative");}
+        else{
+        System.out.println("Not Negative");
+        }
+    
+   
+
+
+
+        }
+        
+    }
