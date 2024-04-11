682df066c4a2d3d90426b4d0ae6a8800e20cc3f3
emmachen12 <159071847+emmachen12@users.noreply.github.com>
2024-04-10 20:25:32 -0400

    Update Game21.java

--- Game21.java
+++ Game21.java
@@ -1,6 +1,8 @@
+//Emma
+// 4-10-24
 import java.util.*;
 public class Game21 {
-    public static void main (String[]args){
+public static void main (String[]args){
     int num1 = (int)(11*Math.random()+1);
     int num2 = (int)(11*Math.random()+1);
     int sum1 = num1 + num2;
@@ -15,13 +17,31 @@ public class Game21 {
         int num4 = (int)(11*Math.random()+1);
         int sum2 = sum1 + num4;
         System.out.println("You got a " + num4 + " for a total of " + sum2 );
-
     }
-    else{
+    else(num3 != 1){
         int num5 = (int)(11*Math.random()+1);
         int num6 = (int)(11*Math.random()+1);
         int num7 = (int)(11*Math.random()+1);
+        int dealsum = num5 + num6 + num7;
+        
+        System.out.println("The dealer has a " + num5 + ", " + num6 + " and " + num7 + " for a total of " + dealsum);
+    }
+    
+    if(sum2 > 21){
+        System.out.println("Player lose");
+    }
+    else if(dealsum > 21){
+        System.out.println("Player wins");
+    }
+    else if(sum2 > dealsum){
+        System.out.println("Player wins");
+    }
+    else if(sum2 <  dealsum){
+        System.out.println("Player loses");
+    }
+    else{
+        System.out.println("Tie");
 
     }
-    }
+}
 }
